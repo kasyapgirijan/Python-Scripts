ef0f8d03190976742f6f4aee444f8f3903e09dde
Kasyap Girijan <104567271+kasyapgirijan@users.noreply.github.com>
2025-05-07 17:04:29 +0530

    Update PSAT.py

--- ProofPoint/PSAT.py
+++ ProofPoint/PSAT.py
@@ -4,32 +4,35 @@ import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
-BASE_URL = "https://results.us.securityeducation.com/api/reporting/v0.3.0/"
-API_KEY = "<your_api_key>"
+with open("api_key.txt", "r") as file:
+    api_key = file.read().strip()
+
+datetime_now = datetime.datetime.now()
+date_string = datetime_now.strftime("Y-%m-%d_%H-%M-%S")
+
+base_url = "https://results.us.securityeducation.com/api/reporting/v0.3.0/"
 
 def extract_user_tags(attributes):
     user_tags_data = {}
     user_tags = attributes.get('usertags', {})
     if user_tags:
         for tag_category, tag_values in user_tags.items():
-            # Renaming logic can be applied here
-            # For example, if you want to replace spaces with underscores and convert to lowercase
-            tag_category = tag_category.lower().replace(' ', '_')
-            user_tags_data.update({f'{tag_category}_{i+1}': value for i, value in enumerate(tag_values)})
+            tag_category = tag_category.lower().replace('_1', '')
+            user_tags_data.update(
+                {f'{tag_category}_{i+1}': value for i, value in enumerate(tag_values)})
     return user_tags_data
 
-def extract_attributes(api_url, report_type, page_size=100):
+def extract_attributes(api_url, report_type):
     extracted_data = []
-
     try:
         page = 1
         while True:
-            url = f"{api_url}&page[number]={page}&page[size]={page_size}"
-            headers = {"Authorization": f"Bearer {API_KEY}"}
+            url = f"{api_url}&page[number]={page}&page[size]=8000&filter[_includedeletedusers]=TRUE"
+            print(url)
+            headers = {"x-apikey-token":f"{api_key}"}
             response = requests.get(url, headers=headers)
             data = response.json()
 
-            # Check if there are records in the current page
             if not data['data']:
                 break  # No more pages
 
@@ -41,50 +44,52 @@ def extract_attributes(api_url, report_type, page_size=100):
                     **attributes
                 }
 
-                # Conditionally include user tags only for 'user' report type
-                if report_type == 'user':
+                if report_type == 'users':
                     item_data.update(extract_user_tags(attributes))
 
                 extracted_data.append(item_data)
-
             page += 1  # Move to the next page
 
     except Exception as e:
-        error_message = f"Error extracting attributes for {report_type}: {e}"
-        print(f"\033[91m{error_message}\033[0m")  # ANSI escape code for red text
+        print(f"Error extracting attributes:{e}")
 
     return extracted_data
 
 def process_report(report_type, workbook):
-    if report_type == 'user':
-        api_url = f"{BASE_URL}{report_type}?filter[user_specific_filter]=value"
+    
+    if report_type == 'users':
+        api_url = f"{base_url}{report_type}?user_tag_enabled&"
     else:
-        api_url = f"{BASE_URL}{report_type}?filter[a]=b"
+        api_url = f"{base_url}{report_type}?"
 
     extracted_data = extract_attributes(api_url, report_type)
 
+    print(f"Collecting {report_type.capitalize()}")
+
     if extracted_data:
-        # Convert the extracted data to a DataFrame
         df = pd.DataFrame(extracted_data)
 
-        # Add the DataFrame as a sheet to the workbook
+        # Remove Rows where uernemail matches sso_id@whateverdomain.com
+        if report_type == 'users':
+            df['sso_id_email'] = df['sso_id'] + '@transunion.com'
+            df = df[~df['useremailaddress'].isin(df['sso_id_email'])]
+
         df.to_excel(workbook, sheet_name=report_type.capitalize(), index=False)
 
         print(f"{report_type.capitalize()} data saved to workbook.")
 
     else:
-        print(f"Failed to retrieve {report_type.capitalize()} data.")
+        print(f"Failed to retrieve attributes for {report_type.capitalize()} data.")
+        return None
 
 def main():
-    report_types = ['training', 'user', 'phishing']  # Add more report types as needed
-
-    # Create a timestamp-based folder
-    timestamp_folder = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    folder_path = os.path.join("./output_data", timestamp_folder)
+    report_types = ['training', 'users', 'phishing']  # Add more report types as needed
+    date_folder = date_string
+    f_path = os.getcwd()
+    folder_path = os.path.join(f_path, date_folder)
     os.makedirs(folder_path, exist_ok=True)
-
     # Create a single workbook
-    workbook_path = os.path.join(folder_path, "reports.xlsx")
+    workbook_path = os.path.join(folder_path, "Proofpoint.xlsx")
     with pd.ExcelWriter(workbook_path, engine='xlsxwriter') as writer:
         with ThreadPoolExecutor(max_workers=3) as executor:
             futures = {executor.submit(process_report, report_type, writer): report_type for report_type in report_types}
